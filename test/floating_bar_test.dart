22d2169966ce6b2bc9cccd2e35c31e99d7b6236f
Sunlution <sunface@outlook.kr>
2024-03-08 17:03:20 +0900

    [UPDATE] targetPlatform added for test code

--- test/floating_bar_test.dart
+++ test/floating_bar_test.dart
@@ -1,10 +1,37 @@
+import 'dart:io';
+
 import 'package:floating_bar/floating_bar.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
+Future<void> _loadFont(String name, String path) async {
+  final file = File(path);
+  final bytes = await file.readAsBytes();
+
+  final fontLoader = FontLoader(name);
+  fontLoader.addFont(Future.value(ByteData.view(bytes.buffer)));
+
+  await fontLoader.load();
+}
+
 void main() async {
-  await loadAppFonts();
+  setUpAll(() async {
+    await _loadFont(
+      '.SF UI Text',
+      'test/fonts/FontsFree-Net-sf-ui-text-regular-58646b56a688c.ttf',
+    );
+    await _loadFont(
+      '.SF UI Display',
+      'test/fonts/FontsFree-Net-SF-UI-Display-Regular-1.ttf',
+    );
+    await _loadFont(
+      'MaterialIcons',
+      'test/fonts/MaterialIcons-Regular.otf',
+    );
+  });
   const firstIcon = Icon(Icons.account_circle);
   const secondIcon = Icon(Icons.date_range);
   const thirdIcon = Icon(Icons.access_alarm);
@@ -21,6 +48,10 @@ void main() async {
 
   // Test for FloatingBar initial look
   testGoldens('FloatingBar initial look', (WidgetTester tester) async {
+    tester.view.physicalSize = const Size(1170, 2532 - 47 * 3 - 34 * 3);
+
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+
     final builder = GoldenBuilder.grid(columns: 2, widthToHeightRatio: 1)
       ..addScenario(
           'isOnLeft: true\ndefault initialYPositionPercentage: 0.8\ndefault floatingBarSize: 50',
@@ -43,10 +74,16 @@ void main() async {
               )));
     await tester.pumpWidgetBuilder(builder.build());
     await screenMatchesGolden(tester, 'floating_bar');
+
+    debugDefaultTargetPlatformOverride = null;
   });
 
   // Test for onTap
   testGoldens('on Tap', (WidgetTester tester) async {
+    tester.view.physicalSize = const Size(1170, 2532 - 47 * 3 - 34 * 3);
+
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+
     final builder = GoldenBuilder.column()
       ..addScenario(
           'expansion when onTap',
@@ -58,10 +95,16 @@ void main() async {
     await tester.pumpWidgetBuilder(builder.build());
     await tester.tap(find.byIcon(Icons.chevron_right));
     await screenMatchesGolden(tester, 'floating_bar_onTap');
+
+    debugDefaultTargetPlatformOverride = null;
   });
 
   // Test for Dragging
   testGoldens('on PanUpdating', (WidgetTester tester) async {
+    tester.view.physicalSize = const Size(1170, 2532 - 47 * 3 - 34 * 3);
+
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+
     final builder = GoldenBuilder.column()
       ..addScenario(
           'dragging when onPanUpdating',
@@ -75,25 +118,31 @@ void main() async {
         tester.state(find.byType(FloatingButtons));
     state.setBoundaryForTest(const Offset(100, 100));
     await screenMatchesGolden(tester, 'floating_bar_onPanUpdated');
+
+    debugDefaultTargetPlatformOverride = null;
   });
 
   // Test for collapsed UI changes
   testGoldens('FloatingBar  collapsed UI changes', (WidgetTester tester) async {
+    tester.view.physicalSize = const Size(1170, 2532 - 47 * 3 - 34 * 3);
+
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+
     final builder = GoldenBuilder.grid(columns: 3, widthToHeightRatio: 1)
       ..addScenario(
           'arrow button color change to red(default : Colors.white)',
           Container(
               color: Colors.green,
-              width: 150,
-              height: 150,
+              width: 100,
+              height: 100,
               child: FloatingBar(
                   expansionButtonColor: Colors.red, children: iconList)))
       ..addScenario(
           'collapsedBackgroundColor change to red(default : Colors.white)',
           Container(
-              color: Colors.blue,
-              width: 150,
-              height: 150,
+              color: const Color.fromARGB(255, 27, 44, 57),
+              width: 100,
+              height: 100,
               child: FloatingBar(
                 collapsedBackgroundColor: Colors.red,
                 children: iconList,
@@ -102,8 +151,8 @@ void main() async {
           'collapsedOpacity change(default : 0.3)',
           Container(
               color: Colors.blue,
-              width: 150,
-              height: 150,
+              width: 100,
+              height: 100,
               child: FloatingBar(
                 collapsedOpacity: 1,
                 collapsedBackgroundColor: Colors.red,
@@ -111,10 +160,16 @@ void main() async {
               )));
     await tester.pumpWidgetBuilder(builder.build());
     await screenMatchesGolden(tester, 'floating_bar_collapsed_UI_changes');
+
+    debugDefaultTargetPlatformOverride = null;
   });
 
   // Test for expanded UI changes
   testGoldens('FloatingBar  expanded UI changes', (WidgetTester tester) async {
+    tester.view.physicalSize = const Size(1170, 2532 - 47 * 3 - 34 * 3);
+
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+
     final builder = GoldenBuilder.column()
       ..addScenario(
           'expanded background color change to red(default : Colors.white)\n'
@@ -130,11 +185,17 @@ void main() async {
     await tester.pumpWidgetBuilder(builder.build());
     await tester.tap(find.byIcon(Icons.chevron_right));
     await screenMatchesGolden(tester, 'floating_bar_expanded_UI_changes');
+
+    debugDefaultTargetPlatformOverride = null;
   });
 
   // Test for expanded UI constraints
   testGoldens('FloatingBar  expanded UI constraints',
       (WidgetTester tester) async {
+    tester.view.physicalSize = const Size(1170, 2532 - 47 * 3 - 34 * 3);
+
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+
     final builder = GoldenBuilder.column()
       ..addScenario(
           'The expansionWidth is determined by parentSize and [expansionWidthPercentage] (default: 0.7).The width of each child in [children] is determined by the expansionWidth and length of [children]',
@@ -161,5 +222,7 @@ void main() async {
     await tester.pumpWidgetBuilder(builder.build());
     await tester.tap(find.byIcon(Icons.chevron_right));
     await screenMatchesGolden(tester, 'floating_bar_expanded_UI_constraints');
+
+    debugDefaultTargetPlatformOverride = null;
   });
 }
